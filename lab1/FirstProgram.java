47002e9ee12c60ca6e7485a420a654e109a3dc24
18SW62 <49367648+18SW62@users.noreply.github.com>
2019-04-13 23:09:24 +0500

    Add files via upload

---
+++ lab1/FirstProgram.java
@@ -0,0 +1,17 @@
+class FirstProgram{
+	public static void main(String[] args) {
+	String name="Abdul Qudoos";
+	int marks=900;
+	float perc=900*100/1000;
+	char grade='A';
+	boolean pass=true;
+	if(true==pass)
+	System.out.println("Status:pass");
+	else 	
+	System.out.println("fail");
+    System.out.println("Name="+name);
+    System.out.println("Marks="+marks);
+    System.out.println("percentage="+perc);
+    System.out.println("Grade="+grade);
+	}
+}
---
+++ lab1/Secondprogram.java
@@ -0,0 +1,10 @@
+class SecondProgram{
+	public static void main(String[] args) {
+		double d=50.25;
+		long l=(long)d;
+		double z=d-l;
+		System.out.println("the actual value is="+d);
+		System.out.println("the integral value is="+l);
+		System.out.println("the fractional value is="+z);
+	}
+}
