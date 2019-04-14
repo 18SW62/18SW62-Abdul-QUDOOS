3037338511cfac8d5ade8082e70b836185968982
18SW62 <49367648+18SW62@users.noreply.github.com>
2019-04-14 11:20:10 +0500

    Add files via upload

--- lab2/FirstProgram.java
+++ lab2/FirstProgram.java
@@ -2,8 +2,6 @@ class FirstProgram{
 	public static void main(String args[]) {
 		String x=args[0];
 		String y=args[1];
-		//int z=Integer.parseInt(x);
-		//int a=Integer.parseInt(y);
 		System.out.println(x+"  "+y);
 	}
 }
